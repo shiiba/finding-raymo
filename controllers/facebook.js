ac6f7a944713edec9c978fe079c1f7dedea30d64
Kevin Shiiba <kevin.shiiba@reaktor.com>
2016-09-26 15:42:09 -0400

    Installing Babel

--- controllers/facebook.js
+++ controllers/facebook.js
@@ -1,8 +1,14 @@
 import express from 'express'
-
 const router = express.Router()
 
 router.get('/', (req, res) => {
   res.send('Hello world, I am a chat bot')
+})
+
+router.get('/webhook/', (req, res) => {
+  if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    res.sens(req.query['hub.challenge'])
+  }
+  res.send('Error, wrong token')
 })
 
