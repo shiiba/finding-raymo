1494bb0b840df9c3576f2247fe7e142280400110
Kevin Shiiba <kevin.shiiba@reaktor.com>
2016-09-28 14:25:19 -0700

    trying client.messages.create

--- controllers/twilio.js
+++ controllers/twilio.js
@@ -8,39 +8,41 @@ const express = require('express'),
 router.use(bodyParser.urlencoded({ extended: true }))
 
 // Twilio creds
-// const accountSid = process.env.TWILIO_SID
-// const authToken = process.env.TWILIO_AUTH_TOKEN
+const accountSid = process.env.TWILIO_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
 // const testAccountSid = process.env.TWILIO_TEST_SID
 // const testAuthToken= process.env.TWILIO_TEST_AUTH_TOKEN
 
 // // require Twilio module
-// const client = require('twilio')(accountSid, authToken)
-  // client.messages.create({
-  //   to: '+19163160342',
-  //   from: '+18474439729',
-  //   body: 'Test message, yo'
-  // }, (err, message) => {
-  //   console.log(message.sid)
-  // })
+const client = require('twilio')(accountSid, authToken)
+
 
 router.post('/', (req, res) => {
-  // Wil respond to you
+  client.messages.create({
+    to: '+19163160342',
+    from: '+18474439729',
+    body: 'Test message, yo'
+  }, (err, message) => {
+    console.log(message.sid)
+  })
+
+  // Will respond to you
   // const twiml = new twilio.TwimlResponse()
   // twiml.message('The Robots are coming! Head for the hills!')
   // res.writeHead(200, {'Content-Type': 'text/xml'})
   // res.end(twiml.toString())
 
   // Conditional Replies
-  const twiml = new twilio.TwimlResponse()
-  if (req.body.Body == 'hello') {
-    twiml.message('Hi!')
-  } else if(req.body.Body == 'bye') {
-    twiml.message('Goodbye')
-  } else {
-    twiml.message('No Body param match, Twilio sends this in the request to your server.')
-  }
-  res.writeHead(200, {'Content-Type': 'text/xml'})
-  res.end(twiml.toString())
+  // const twiml = new twilio.TwimlResponse()
+  // if (req.body.Body == 'hello') {
+  //   twiml.message('Hi!')
+  // } else if(req.body.Body == 'bye') {
+  //   twiml.message('Goodbye')
+  // } else {
+  //   twiml.message('No Body param match, Twilio sends this in the request to your server.')
+  // }
+  // res.writeHead(200, {'Content-Type': 'text/xml'})
+  // res.end(twiml.toString())
 })
 
 module.exports = router
