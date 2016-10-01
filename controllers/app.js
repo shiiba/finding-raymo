be2761d5e4117e41f9559e7a4188ffb915531105
Kevin Shiiba <kevin.shiiba@reaktor.com>
2016-10-01 10:10:34 -1000

    testing text triggered email

--- controllers/app.js
+++ controllers/app.js
@@ -16,17 +16,50 @@ const authToken = 'f4662525b8b264f3a37221602ba094a8'
 // // require Twilio module
 const client = require('twilio')(accountSid, authToken)
 
+// ==========
+// MAILJET
+// ==========
+
+const mailjetApiKey = process.env.MAILJET_API_KEY
+const mailjetSecret = process.env.MAILJET_SECRET_KEY
+
+const Mailjet = require('node-mailjet').connect(mailjetApiKey, mailjetSecret)
+
+const sendEmail = Mailjet.post('send')
+
+const handleError = (err) => {
+  throw new Error(err.ErrorMessage);
+}
+
+const handlePostResponse = (response) => {
+  console.log(response)
+}
 
 router.post('/', (req, res) => {
   console.log(req.body)
+  const messageBody = req.body
+  const messageText = messageBody.Body
 
-  // client.messages.create({
-  //   to: '+19163160342',
-  //   from: '+18474439729',
-  //   body: 'Test message, yo'
-  // }, (err, message) => {
-  //   console.log(message.sid)
-  // })
+  client.messages.create({
+    to: '+19163160342',
+    from: '+18474439729',
+    body: messageText
+  }, (err, message) => {
+    console.log(message.sid)
+  })
+
+  const emailData = {
+    'FromEmail': 'finding.raymond@gmail.com',
+    'FromName': 'Eri Berry',
+    'Subject': messageText,
+    'Recipients': [{'Email': 'kshiiba@gmail.com'}]
+  }
+
+  sendEmail
+    .request(emailData)
+    .then(handlePostResponse)
+    .catch(handleError)
+
 
   // Will respond to you
   // const twiml = new twilio.TwimlResponse()
@@ -46,38 +79,4 @@ router.post('/', (req, res) => {
   // res.writeHead(200, {'Content-Type': 'text/xml'})
   // res.end(twiml.toString())
 })
-
-
-// ==========
-// MAILJET
-// ==========
-
-const mailjetApiKey = process.env.MAILJET_API_KEY
-const mailjetSecret = process.env.MAILJET_SECRET_KEY
-
-const Mailjet = require('node-mailjet').connect(mailjetApiKey, mailjetSecret)
-
-const sendEmail = Mailjet.post('send')
-
-const emailData = {
-  'FromEmail': 'finding.raymond@gmail.com',
-  'FromName': 'Eri Berry',
-  'Subject': 'Test with the NodeJS Mailjet wrapper',
-  'Text-part': 'Hello NodeJs !',
-  'Recipients': [{'Email': 'kshiiba@gmail.com'}]
-}
-
-const handleError = (err) => {
-  throw new Error(err.ErrorMessage);
-}
-
-const handlePostResponse = (response) => {
-  console.log(response)
-}
-
-// sendEmail
-//   .request(emailData)
-//   .then(handlePostResponse)
-//   .catch(handleError)
-
 module.exports = router
